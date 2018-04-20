6252e528030ef8a34a8d2c41471e74d0f109d062
nuest <daniel.nuest@uni-muenster.de>
2018-04-20 15:15:10 +0200

    install required latex packages for PDF output

--- install.R
+++ install.R
@@ -1,6 +1,15 @@
+# Install CRAN packages
 install.packages("pdftools")
 install.packages("tidytext")
 install.packages("wordcloud")
 install.packages("ggthemes")
 install.packages("kableExtra")
 install.packages("huxtable")
+
+# Install LaTeX packages
+tinytex::tlmgr_install("siunitx")
+tinytex::tlmgr_install("xcolor")
+tinytex::tlmgr_install("colortbl")
+tinytex::tlmgr_install("multirow")
+tinytex::tlmgr_install("lscape")
+tinytex::tlmgr_install("wrapfig")
